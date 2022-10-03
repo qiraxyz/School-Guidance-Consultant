0b66d1bddb692af4c7f25166d455c9be9bcd28d8
qiraxyz <ariqaziz74@gmail.com>
2022-10-03 10:23:54 +0700

    push

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+
+    $shock = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "layanan"; 
+
+    $connect = mysqli_connect($shock,$user,$pass,$db) or die ("error");
+?>
---
+++ html/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+    $shock = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "layanan";
+
+    $connect = mysqli_connect($shock,$user,$pass,$db)or
+    die ("error");
+
+?>
